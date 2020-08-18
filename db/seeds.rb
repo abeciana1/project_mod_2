5e675fa4a7d7b8724647094d175477bce952100a
redazures <jwu_nyc@yahoo.com>
2020-08-18 00:05:24 -0400

    stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,9 @@ users.each{|u|User.create(name:u)}
     Pergola.create(name: Faker::Games::Witcher.location)
 end
 
-60,times do
-    Book.create(count:rand(20..50),book_record_id:BookRecord.all.sample.id,pergola_id:Pergola.all.sample.id,user_id:User.all.sample.id)
-end
+# 60,times do
+#     Book.create(count:rand(20..50),book_record_id:BookRecord.all.sample.id,pergola_id:Pergola.all.sample.id,user_id:User.all.sample.id)
+# end
 
 p BookRecord.count
 p Pergola.count
